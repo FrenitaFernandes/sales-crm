a44e38f8ecb18fe003e1abb9aa1e82faa7bb69aa
Glanith Dcunha <dcunhaglanith@gmail.com>
2026-03-04 10:19:08 +0530

    Fix duplicate import declarations

--- src/pages/admin/sales/Project/Projects.jsx
+++ src/pages/admin/sales/Project/Projects.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { MdAdd, MdClose } from "react-icons/md";
-
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Projects() {
--- src/pages/customer/CustomizeProject.jsx
+++ src/pages/customer/CustomizeProject.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function CustomizeProject() {
